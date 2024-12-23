c60031c460f524eecb53c570d83f029540952a68
Alejandro Garcia <garcialejan@gmail.com>
2024-12-23 22:16:47 +0100

    Multivariate model added

--- Theory_notebooks/helper_functions.py
+++ Theory_notebooks/helper_functions.py
@@ -394,7 +394,8 @@ def mean_absolute_scaled_error(y_true, y_pred):
   # Find MAE of naive forecast (no seasonality)
   mae_naive_no_season = tf.reduce_mean(tf.abs(y_true[1:] - y_true[:-1])) # our seasonality is 1 day (hence the shifting of 1 day)
 
-  return mae / mae_naive_no_season
+  return mae / mae_naive_no_seasons
+
 
 def evaluate_preds(y_true, y_pred):
   # Make sure float32 (for metric calculations)
